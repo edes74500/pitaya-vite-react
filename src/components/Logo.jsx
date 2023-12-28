38f4a23d8ad916ddc73e9ffca0e0f5a927c41586
KLEIN Jean-Daniel <kleinjeandaniel@gmail.com>
2023-12-29 00:19:07 +0100

    4

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 // import React from "react";
 import { NavLink } from "react-router-dom";
 
-const Logo = () => {
+const Logo = ({ navFixed }) => {
   return (
     <div className="logo-container">
       <div className="logo-text-container">
@@ -10,7 +10,7 @@ const Logo = () => {
           <h2>PITAYA</h2>
         </NavLink>
       </div>
-      <div className="logo-img-container">
+      <div className={`logo-img-container ${navFixed && "on-scroll"}`}>
         <NavLink to="/">
           <img src="./img/logo2.png" alt="pitaya-logo2" />
         </NavLink>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,31 @@
-// import React from "react";
+import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
 
 const Navigation = () => {
+  const [navHeight, setNavHeight] = React.useState(0);
+  const [navFixed, setNavFixed] = React.useState(false);
+
+  const navigation = document.getElementsByClassName("navigation");
+
+  useEffect(() => {
+    setNavHeight(navigation[0].offsetHeight);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", (e) => {
+      if (window.scrollY > navHeight) {
+        setNavFixed(true);
+      } else {
+        setNavFixed(false);
+      }
+    });
+  }, []);
+
   return (
-    <div className="navigation">
+    <div className={`navigation ${navFixed && "fixed-navigation"}`}>
       <div className="nav-container">
-        <Logo />
+        <Logo navFixed={navFixed} />
         <ul>
           <li>
             <NavLink to="/" className={(nav) => (nav.isActive ? "is-active" : "")}>
